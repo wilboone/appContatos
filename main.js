0fca17d9d060a82bbb4c807b1a3bcfb140988a46
Wilson Boone <wilson.boone@gmail.com>
2026-03-25 14:44:38 -0400

    adicionado mascaras js

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 "strict mode";
 
 console.log("Teste JS");
+
+document.getElementById("cpf").addEventListener("input", function (e) {
+  let value = e.target.value.replace(/\D/g, "");
+
+  if (value.length > 11) value = value.slice(0, 11);
+
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d)/, "$1.$2");
+  value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+  e.target.value = value;
+});
+
+// function aplicarMascaraCelular(input) {
+//   input.addEventListener("input", function (e) {
+//     let value = e.target.value.replace(/\D/g, "");
+
+//     if (value.length > 11) value = value.slice(0, 11);
+
+//     value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
+//     value = value.replace(/(\d{5})(\d)/, "$1-$2");
+
+//     e.target.value = value;
+//   });
+// }
+
+// document.querySelectorAll(".celular").forEach(aplicarMascaraCelular);
+
+document.getElementById("celular").addEventListener("input", function (e) {
+  let value = e.target.value.replace(/\D/g, "");
+
+  if (value.length > 11) value = value.slice(0, 11);
+
+  value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
+  value = value.replace(/(\d{5})(\d)/, "$1-$2");
+
+  e.target.value = value;
+});
