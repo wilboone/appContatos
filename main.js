4c9b726739bee2998e40cea35f7058b4eaeac990
Wilson Boone <wilson.boone@gmail.com>
2026-03-26 19:41:16 -0400

    add numeros

--- main.js
+++ main.js
@@ -14,28 +14,38 @@ document.getElementById("cpf").addEventListener("input", function (e) {
   e.target.value = value;
 });
 
-// function aplicarMascaraCelular(input) {
-//   input.addEventListener("input", function (e) {
-//     let value = e.target.value.replace(/\D/g, "");
+function mascara(input) {
+  input.addEventListener("input", function (e) {
+    let value = e.target.value.replace(/\D/g, "");
 
-//     if (value.length > 11) value = value.slice(0, 11);
+    if (value.length > 11) value = value.slice(0, 11);
 
-//     value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
-//     value = value.replace(/(\d{5})(\d)/, "$1-$2");
+    value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
 
-//     e.target.value = value;
-//   });
-// }
+    e.target.value = value;
+  });
+}
 
-// document.querySelectorAll(".celular").forEach(aplicarMascaraCelular);
+document.querySelectorAll(".celular").forEach(mascara);
 
-document.getElementById("celular").addEventListener("input", function (e) {
-  let value = e.target.value.replace(/\D/g, "");
+document.getElementById("addTelefone").addEventListener("click", function () {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("input-group", "mb-2");
+  newDiv.innerHTML = `<input type="text" id="celular" class="form-control celular" placeholder="(00) 00000-0000" maxlength="15" required/>
+              <button type="button" class="btn btn-danger remover">Remover</button>`;
 
-  if (value.length > 11) value = value.slice(0, 11);
+  mascara(newDiv.querySelector(".celular"));
 
-  value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
-  value = value.replace(/(\d{5})(\d)/, "$1-$2");
+  telefones.appendChild(newDiv);
 
-  e.target.value = value;
+  const tel = document.getElementById("telefones");
+  let total = tel.querySelectorAll(".input-group").length;
+  if (total >= 10) {
+    document.getElementById("addTelefone").classList.add("d-none");
+    return;
+  }
+
+  console.log(total);
+  console.log(tel);
 });
