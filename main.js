f21dfc3ac8f7af25961acff1a4b4cb99fd6d35f7
Leonardo C Dantas <leochecon@checon.com.br>
2026-03-27 10:42:30 -0300

    add exemplo sobre modificação de inputs

--- main.js
+++ main.js
@@ -29,6 +29,27 @@ document.getElementById("cpf").addEventListener("input", function (e) {
 // 
 // document.querySelectorAll(".celular").forEach(mascara);
 
+
+$(function () {
+  function formatarTelefone(value) {
+    let formatedValue = value.replace(/\D/g, "");
+    // Eu costumo evitar modificar o valor original recebido na função.
+    // Quando é um objeto complexo, isso pode causar efeitos colaterais indesejados, e mesmo com tipos primitivos, é uma boa prática manter a imutabilidade.
+
+    formatedValue = formatedValue.slice(0, 11)
+      .replace(/^(\d{2})(\d)/g, "($1) $2")
+      .replace(/(\d{5})(\d)/, "$1-$2");
+    return formatedValue;
+  }
+
+  $(".celular").on("input", function (e) {
+    const $element = $(e.target);
+    const val = $element.val();
+    // Seria possível ajustar a função para receber o elemento diretamente e isso limparia mais a escrita, mas mantive para seguir a mesma ideia do código alterado
+    $element.val(formatarTelefone(val));
+  });
+})
+
 document.getElementById("addTelefone").addEventListener("click", function () {
   const newDiv = document.createElement("div");
   newDiv.classList.add("input-group", "mb-2");
@@ -50,18 +71,30 @@ document.getElementById("addTelefone").addEventListener("click", function () {
   console.log(tel);
 });
 
-$(function () {
-  function formatarTelefone(value) {
-    let formatedValue = value.replace(/\D/g, "");
-    formatedValue = formatedValue.slice(0, 11);
-    formatedValue = formatedValue.replace(/^(\d{2})(\d)/g, "($1) $2");
-    formatedValue = formatedValue.replace(/(\d{5})(\d)/, "$1-$2");
-    return formatedValue;
-  }
 
-  $(".celular").on("input", function (e) {
-    const $element = $(e.target);
-    const val = $element.val();
-    $element.val(formatarTelefone(val));
-  });
-})
+// Exemplo: possíveis efeitos colaterais decorrentes da modificação do input
+function changeSimpleInput(simpleString) { 
+  simpleString += simpleString
+  return simpleString;
+}
+
+let myString = "Hello!"
+console.log("1x changeSimpleInput(myString):")
+console.log(changeSimpleInput(myString))
+console.log("2x changeSimpleInput(myString):")
+console.log(changeSimpleInput(myString))
+console.log("3x changeSimpleInput(myString):")
+console.log(changeSimpleInput(myString))
+
+function changeComplexInput(someDict) {
+  someDict["hello"] += someDict["hello"];
+  return someDict;
+}
+
+let myDict = {"hello": "Hello!"}
+console.log("1x changeComplexInput(myDict):")
+console.log(changeComplexInput(myDict))
+console.log("2x changeComplexInput(myDict):")
+console.log(changeComplexInput(myDict))
+console.log("3x changeComplexInput(myDict):")
+console.log(changeComplexInput(myDict))
